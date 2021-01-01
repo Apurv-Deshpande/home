aa6d3a6ede78ec8a2e5e63aaee364690573820be
Apurv-Deshpande <apurvdeshpande02@gmail.com>
2021-01-01 11:37:28 +0530

    Updates

---
+++ precache-manifest.009699c83d794fb0bb74914d920655a4.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3aa92bffa206177e8f817518bdf20c",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "e22c73022a4b9a0d41b8",
+    "url": "/home/static/css/main.efa476c1.chunk.css"
+  },
+  {
+    "revision": "46e16794679887244475",
+    "url": "/home/static/js/2.760b03de.chunk.js"
+  },
+  {
+    "revision": "e22c73022a4b9a0d41b8",
+    "url": "/home/static/js/main.d16e5ec3.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "bdbfe81afe77c41e52a0e8365b10151e",
+    "url": "/home/static/media/aws.bdbfe81a.png"
+  },
+  {
+    "revision": "ff3b5455698166bd6e516652bcc3b192",
+    "url": "/home/static/media/azure.ff3b5455.png"
+  },
+  {
+    "revision": "c5fb75bf9bafdaf9d004cd58ae5317bf",
+    "url": "/home/static/media/bootstrap.c5fb75bf.png"
+  },
+  {
+    "revision": "dac5475c76b98073401605c3cd34a430",
+    "url": "/home/static/media/c.dac5475c.png"
+  },
+  {
+    "revision": "aa436d45567a403b52a87e3372271189",
+    "url": "/home/static/media/course-angular.aa436d45.png"
+  },
+  {
+    "revision": "1ae939eb7add89efd2ce4a76f7ff97e5",
+    "url": "/home/static/media/course.1ae939eb.png"
+  },
+  {
+    "revision": "1aac5a58221c52d3abc8587bb50d2d41",
+    "url": "/home/static/media/cpp.1aac5a58.png"
+  },
+  {
+    "revision": "1e08a2fe2853345e51658165033fac40",
+    "url": "/home/static/media/css.1e08a2fe.png"
+  },
+  {
+    "revision": "be9c30242b2cfc3eb00a5533381b4018",
+    "url": "/home/static/media/database.be9c3024.png"
+  },
+  {
+    "revision": "b7bd4af00a7f0c8115862d3953ef6a11",
+    "url": "/home/static/media/datastructures.b7bd4af0.png"
+  },
+  {
+    "revision": "039b43b870ed39270fbf2cf19475fc89",
+    "url": "/home/static/media/engineering.039b43b8.png"
+  },
+  {
+    "revision": "effab7e37123866a1c2db93e793b4b89",
+    "url": "/home/static/media/excel.effab7e3.png"
+  },
+  {
+    "revision": "81f1e720ba7ec45db08a1eeb8d96a801",
+    "url": "/home/static/media/football.81f1e720.png"
+  },
+  {
+    "revision": "41563aeeedce92de4fc34986e2b8d710",
+    "url": "/home/static/media/hadoop.41563aee.png"
+  },
+  {
+    "revision": "d38343df72da322b7231ade8b0dcb215",
+    "url": "/home/static/media/hive.d38343df.png"
+  },
+  {
+    "revision": "92acc939e81266cf6eb487029b7c15d0",
+    "url": "/home/static/media/html5.92acc939.png"
+  },
+  {
+    "revision": "eddb42e065a69e50c8747cc91267165a",
+    "url": "/home/static/media/java.eddb42e0.png"
+  },
+  {
+    "revision": "837d97363d05519d8d4dcbb3ea092a69",
+    "url": "/home/static/media/javascript.837d9736.png"
+  },
+  {
+    "revision": "ea24508b05f4c2f2b3a49ddb3cb2e41e",
+    "url": "/home/static/media/maths.ea24508b.png"
+  },
+  {
+    "revision": "e5eee315a17de0d7f56117077eb71fa9",
+    "url": "/home/static/media/mongodb.e5eee315.png"
+  },
+  {
+    "revision": "de06753eee667bda65b5556d06b26a9d",
+    "url": "/home/static/media/mysql.de06753e.png"
+  },
+  {
+    "revision": "3c62c85882d8d9c020ebf06fe44afa44",
+    "url": "/home/static/media/networking.3c62c858.png"
+  },
+  {
+    "revision": "428ade38b504be66a6590e1daef52b7a",
+    "url": "/home/static/media/nodejs.428ade38.png"
+  },
+  {
+    "revision": "4eb39ab00d7614590aa80ec10efcfd27",
+    "url": "/home/static/media/portfolio.4eb39ab0.png"
+  },
+  {
+    "revision": "92b8268b1010c46c076b6018de23a476",
+    "url": "/home/static/media/react.92b8268b.png"
+  },
+  {
+    "revision": "248cb156cf25bd359ef4705fb4306762",
+    "url": "/home/static/media/reactnative.248cb156.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/home/static/media/redux.8251fe76.png"
+  },
+  {
+    "revision": "2b0c63db7e1e38f448ec1fb58cb06094",
+    "url": "/home/static/media/sass.2b0c63db.png"
+  },
+  {
+    "revision": "c69cef8344e33ee65207421feb2fe9d3",
+    "url": "/home/static/media/statistics.c69cef83.png"
+  },
+  {
+    "revision": "c193c6383b80a26da9e68d7bd90c2263",
+    "url": "/home/static/media/webdevelopment.c193c638.png"
+  }
+]);
